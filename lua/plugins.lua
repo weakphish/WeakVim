8325e73dadbac141b3f7d1235da3af52c1de0a5c
John Allison <john123allison@gmail.com>
2022-05-26 16:51:42 -0400

    Add dependency that was breaking nvim-cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function()
         }
     }
     use 'hrsh7th/nvim-cmp'              -- Autocompletion plugin
+    use 'hrsh7th/vim-vsnip'             -- Dependency for above features
     use 'hrsh7th/cmp-nvim-lsp'          -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip'      -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip'              -- Snippets plugin
